1c475a56731566a355e7690e0540a548a2d30fe1
Gonzalo Alvarez Campos <gonzaloralvarezcampos@gmail.com>
2022-04-21 13:36:48 -0300

    unreviewed code deleted

--- index.js
+++ index.js
@@ -9,39 +9,3 @@ process.stdout.write("Sending request\n");
 })();
 
 process.exitCode = 0;
-
-/*
-curl \
-  -H "Accept: application/vnd.github.v3+json" \
-  https://api.github.com/repos/moove/apprenticeship
-
-Get org info
-const fetchData = await octokit.request('GET /orgs/{org}/projects', {
-    org: 'moove-it',
-  });
-
-Get repo
-const fetchData = await octokit.request('GET /repos/{owner}/{repo}', {
-    owner: 'moove-it',
-    repo: 'apprenticeship',
-  });
-
-Get projects from specific repo
-  const fetchData = await octokit.request('GET /repos/{owner}/{repo}/projects', {
-    owner: 'Alvacampos',
-    repo: 'apprenticeship',
-  });
-
-   if (element) {
-      try {
-        const fetchProjectsData = await octokit.request('GET /repos/{owner}/{repo}/projects', {
-          owner: 'Alvacampos',
-          repo: 'apprenticeship',
-        });
-      } catch (e) {
-        console.log(e);
-        process.exitCode = 0;
-      }
-    }
-
-*/
